b3762aea8f71fc8a303bcaf43aa1fa0f54e662f9
Pedro Martins <github@pedromartins.pt>
2019-02-09 19:44:45 +0000

    Grid de Histórico

--- Client/src/views/base/ClienteTab2.vue
+++ Client/src/views/base/ClienteTab2.vue
@@ -1,15 +1,45 @@
 <template>
-  <div>
-      TAB 2
+  <div class="animated fadeIn">
+    <b-row>
+      <b-col lg="12">
+        <c-table
+          :table-data="items"
+          :fields="fields"
+          :perPage="5"
+          caption="<i class='fa fa-align-justify'></i> Histórico"
+        ></c-table>
+      </b-col>
+    </b-row>
+    <!--/.row-->
   </div>
 </template>
 
 <script>
-  export default {
-    name: "ClienteTab2",
+import axios from "axios";
+import cTable from "./Table.vue";
+
+export default {
+  name: "clientes",
+  components: { cTable },
+  data: () => {
+    return {
+      items: [],
+      fields: [
+        { key: "descricao", label: "Descrição", sortable: true },
+        { key: "dataHora", sortable: true },
+      ]
+    };
+  },
+  created: function() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function() {
+      await axios
+        .get(`http://localhost:8181/api/clientes/${this.$route.params.id}/historico`)
+        .then(response => (this.items = response.data))
+        .catch(error => alert("Ocorreu um erro ao obter a informação."));
+    }
   }
+};
 </script>
-
-<style scoped>
-
-</style>
